f018358c8f4f224d097f76e6f85336c13350a460
sara206 <71878182+saramohile@users.noreply.github.com>
2026-06-05 03:00:32 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if(s.length() != t.length()){
+            return false;
+        }
+        //using hashmap
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char c: s.toCharArray()){
+            if(map.containsKey(c)){
+                map.put(c , map.get(c)+1);
+            }
+            else map.put(c,1);
+        }
+
+        for(char c: t.toCharArray()){
+            if(!map.containsKey(c)){
+                return false;
+            }
+            map.put(c, map.get(c)-1);
+
+            if(map.get(c) < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
